4402b102ac6372802344479b5d4ebf9020832e5b
Sidharthpr <sidharthprsidhu@gmail.com>
2025-02-12 08:46:50 +0530

    .env and gitignore added

--- MySecondProject/settings.py
+++ MySecondProject/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'MySecondProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'evfinderdb',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
